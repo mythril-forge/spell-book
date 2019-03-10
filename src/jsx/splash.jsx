2559c11cf8f0a677ea32a1506f7aa06c5dd7696c
Nolan Kovacik <noltron000@gmail.com>
2019-03-10 04:16:34 -0700

    adding octocat button

--- src/jsx/splash.jsx
+++ src/jsx/splash.jsx
@@ -4,13 +4,15 @@ import { BrowserRouter as Route, Link } from "react-router-dom";
 
 // import other local assets
 import logoSmall from '../img/logo-small.svg'
+import octocat from '../img/octocat.svg'
 
 // define component item
 const Splash = () => {
 	return (
 		<div>
 			<div id='hero'>
-				<img id='brandproof' src={logoSmall} alt='D&amp;D 5e'/>
+				<a href='http://dnd.wizards.com/dungeons-and-dragons/what-is-dd'><img id='brandproof' src={logoSmall} alt='D&amp;D 5e'/></a>
+				<a href='https://github.com/noltron000/spell-book'><img id='gitproof' src={octocat} alt='made with github' /></a>
 				<h1>Lost&emsp;Arts</h1>
 				<Link to='./books/new'><span tabIndex='0' className='button'>Craft Spellbook</span></Link>
 			</div>
