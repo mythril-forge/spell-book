2e9f83d64373b864d598182772f50a21870c88be
Nolan Kovacik <noltron000@gmail.com>
2019-03-10 03:21:16 -0700

    📸 adding hero images

--- src/jsx/splash.jsx
+++ src/jsx/splash.jsx
@@ -2,12 +2,18 @@
 import React from 'react'
 import { BrowserRouter as Route, Link } from "react-router-dom";
 
+// import other local assets
+import logoSmall from '../img/logo-small.svg'
+
 // define component item
 const Splash = () => {
 	return (
-		<div id='hero'>
-			<h1>Witty Title</h1>
-			<Link to='./books'><span tabIndex='0' className='button'>Craft Spellbook</span></Link>
+		<div>
+			<div id='hero'>
+				<img id='brandproof' src={logoSmall} alt='D&amp;D 5e'/>
+				<h1>Lost&emsp;Arts</h1>
+				<Link to='./books'><span tabIndex='0' className='button'>Craft Spellbook</span></Link>
+			</div>
 		</div>
 	)
 }
