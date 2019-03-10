83b6c359e138c09fdea54e7164bb7f77cdc2df3f
Nolan Kovacik <noltron000@gmail.com>
2019-03-09 19:30:17 -0800

    removing semicolons

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from '../img/logo.svg';
-import '../css/app.css';
+import React, { Component } from 'react'
+import logo from '../img/logo.svg'
+import '../css/app.css'
 
 class App extends Component {
 	render() {
@@ -21,8 +21,8 @@ class App extends Component {
 					</a>
 				</header>
 			</div>
-		);
+		)
 	}
 }
 
-export default App;
+export default App
