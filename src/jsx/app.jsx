775393da30a63c054ab680aa43d26f11d340053d
Nolan Kovacik <noltron000@gmail.com>
2019-03-11 15:18:38 -0700

    adding social media buttons on splash

--- src/jsx/app.jsx
+++ src/jsx/app.jsx
@@ -2,6 +2,12 @@
 import React, { Component } from 'react'
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faDAndD } from '@fortawesome/free-brands-svg-icons'
+
 
 // import components
 import Splash from './splash.jsx'
@@ -12,6 +18,12 @@ import EditSpellbook from './spellbooks-edit.jsx'
 
 // import other local assets
 import '../css/app.css'
+
+// init font-awesome assets
+library.add(faFacebookF)
+library.add(faTwitter)
+library.add(faGithub)
+library.add(faDAndD)
 
 // define component item
 class App extends Component {
--- src/jsx/splash.jsx
+++ src/jsx/splash.jsx
@@ -2,15 +2,17 @@
 import React from 'react'
 // eslint-disable-next-line no-unused-vars
 import { BrowserRouter as Route, Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 // import other local assets
-import dndSmall from '../img/dnd-small.svg'
-import octocat from '../img/octocat.svg'
+import dndAmp from '../img/dnd-amp.svg'
+import github from '../img/github.svg'
 
 // define component item
 const Splash = () => {
 	return (
 		<div>
+
 			<div id='hero' />
 
 			<div id='home'>
@@ -19,18 +21,21 @@ const Splash = () => {
 				<Link to='./books' tabIndex='0' className='button'>Get Started</Link>
 
 				<a href='https://github.com/noltron000/spell-book'>
-					<img className='git proof' src={octocat} alt='made with github' />
+					{/* would use font-awesome but cant use vh+vw w/fonts in css */}
+					<img className='git logo' src={github} alt='made with github' />
 				</a>
 
 				<a href='http://dnd.wizards.com/dungeons-and-dragons/what-is-dd'>
-					<img className='dnd proof' src={dndSmall} alt='for d&amp;d 5e' />
+					{/* would use font-awesome but cant use vh+vw w/fonts in css */}
+					<img className='dnd logo' src={dndAmp} alt='for d&amp;d 5e' />
 				</a>
 			</div>
 
 			<footer id='info'>
-				<p>twitter</p>
-				<p>facebook</p>
-				<p>about</p>
+				<p><a href='https://facebook.com' className='facebook social-media'><FontAwesomeIcon icon={['fab', 'facebook-f']} /> Facebook</a></p>
+				<p><a href='https://twitter.com' className='twitter social-media'><FontAwesomeIcon icon={['fab', 'twitter']} /> Twitter</a></p>
+				<span />
+				<p><Link to='/about' className='social-media'>About</Link></p>
 			</footer>
 
 		</div>
